7c82b3ed3cfb32fe426f91aa89530d286e456425
harunbd80 <harun708280@gmail.com>
2024-05-11 03:43:48 +0600

    'uodate,

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request):
+    return render(request,'index.html')
+
+def registration(request):
+    return render(request)
+
+def About(request):
+    return render(request,'about.html')
+
+def service(request):
+    return render(request,'service.html')
